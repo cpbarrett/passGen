5c493b538e5d18c20adc611c44c63baa498512f6
cpbarrett <cpbarrett@gmail.com>
2019-12-16 15:32:53 -0800

    style changes for mobile

--- js/generator.js
+++ js/generator.js
@@ -62,12 +62,12 @@ function copyPassword(){
 
 	/* Select the text field */
 	copyText.select();
-	copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+	copyText.setSelectionRange(0, 33); /*For mobile devices*/
 
 	/* Copy the text inside the text field */
 	document.execCommand("copy");
 
 	/* Alert the copied text */
-	alert("Copied the text: " + copyText.value);
+	alert("Copied: " + copyText.value);
 
 }
